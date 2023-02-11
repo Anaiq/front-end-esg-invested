8ece11353ec48573bc1c720a775d82790e2d60e2
Anaiq <>
2023-02-11 17:53:06 -0600

    finished login and registration design

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,7 +6,6 @@ import "./Login.css";
 import LoginHeader from './LoginHeader';
 import Footer from './Footer';
 
-
 const kDefaultFormState = {
     username: '',
     password: ''
@@ -35,16 +34,14 @@ const Login: React.FunctionComponent<ILoginProps> = ({ handleLoginSubmit}) => {
 
     const handleSubmit = (event:any): void => {
     event.preventDefault()
-    if (!loginFormData.username || !loginFormData.password ) return;
+    if (!loginFormData.username || !loginFormData.password) return;
     if (loginFormData === undefined) {
         return;
     } else {
         handleLoginSubmit(loginFormData)
         navigate('/portfolio');
     }
-
-    
-    }
+    };
 
     return (
         <div className="container">
@@ -60,61 +57,61 @@ const Login: React.FunctionComponent<ILoginProps> = ({ handleLoginSubmit}) => {
                 <div className="col"></div>
                 <div className="mb-3 col-4 login-form-container">
                     <form className="login-form text-primary" onSubmit={handleSubmit}>
-                        <label className="form-label" htmlFor='username' >Username</label>
+                        <label className="form-label" htmlFor="username" >Username</label>
                         <input type="text" id="username" value={loginFormData.username} onChange={handleChange} placeholder="Username" name="username"></input>
                         <label className="form-label" htmlFor="password">Password</label>
                         <input type="password" id="password" value={loginFormData.password} onChange={handleChange} placeholder="********" name="password"></input>
                         <button className="btn btn-primary"  type="submit" value="Log In" >Login</button>
                     </form>
                 </div>
-                <div className="col"></div>
-            </div>
+            <div className="col"></div>
+        </div>
             
-            <div className="col mb-5 text-center">
-                <Link to='/register'>Need an Account? Register Here.</Link>
-            </div>
-            
-            <div className="row  mb-5">
-                <div className="col">
-                    <div className="card border-light mb-3" >
-                        <div className="card-header text-center">E</div>
-                        <div className="card-body">
-                            <h4 className="card-title">Environment</h4>
-                            <p className="card-text"> "Environment issues may include corporate climate policies, energy use,
-                                waste, pollution, natural resource conservation, and treatment of animals." (source investopedia)
-                            </p>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">Publishes a carbon or sustainability report</li>
-                                <li className="list-group-item">Limits harmful pollutants and chemicals</li>
-                                <li className="list-group-item">Seeks to lower greenhouse gas emissions and CO2 footprint</li>
-                                <li className="list-group-item">Uses renewable energy sources and reduces waste</li>
-                            </ul> 
-                        </div>
+        <div className="col mb-5 text-center">
+            <Link to='/register'>Need an Account? Register Here.</Link>
+        </div>
+        
+        <div className="row  mb-5">
+            <div className="col">
+                <div className="card border-light mb-3" >
+                    <div className="card-header text-center">E</div>
+                    <div className="card-body">
+                        <h4 className="card-title">Environment</h4>
+                        <p className="card-text"> "Environment issues may include corporate climate policies, energy use,
+                            waste, pollution, natural resource conservation, and treatment of animals." (source investopedia)
+                        </p>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Publishes a carbon or sustainability report</li>
+                            <li className="list-group-item">Limits harmful pollutants and chemicals</li>
+                            <li className="list-group-item">Seeks to lower greenhouse gas emissions and CO2 footprint</li>
+                            <li className="list-group-item">Uses renewable energy sources and reduces waste</li>
+                        </ul> 
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card border-light mb-3" >
-                        <div className="card-header text-center">S</div>
-                        <div className="card-body">
-                            <h4 className="card-title">Social</h4>
-                            <p className="card-text">
-                            "Social aspects look at the company’s relationships with internal and external stakeholders."
-                            (source investopedia)
-                            </p>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">Operates an ethical supply chains</li>
-                                <li className="list-group-item">Has a high regard for employees’ health and safety
-                                    or avoids companies that employ child labor</li>
-                                <li className="list-group-item">Supports LGBTQ+ rights and encourages all forms of diversity</li>
-                                <li className="list-group-item">Has policies to protect against sexual misconduct</li>
-                                <li className="list-group-item">Pays fair (living) wages</li>
-                            </ul> 
-                        </div>
+            </div>
+            <div className="col">
+                <div className="card border-light mb-3" >
+                    <div className="card-header text-center">S</div>
+                    <div className="card-body">
+                        <h4 className="card-title">Social</h4>
+                        <p className="card-text">
+                        "Social aspects look at the company’s relationships with internal and external stakeholders."
+                        (source investopedia)
+                        </p>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Operates an ethical supply chains</li>
+                            <li className="list-group-item">Has a high regard for employees’ health and safety
+                                or avoids companies that employ child labor</li>
+                            <li className="list-group-item">Supports LGBTQ+ rights and encourages all forms of diversity</li>
+                            <li className="list-group-item">Has policies to protect against sexual misconduct</li>
+                            <li className="list-group-item">Pays fair (living) wages</li>
+                        </ul> 
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card border-light mb-3" >
-                        <div className="card-header text-center">G</div>
+            </div>
+            <div className="col">
+                <div className="card border-light mb-3" >
+                    <div className="card-header text-center">G</div>
                         <div className="card-body">
                             <h4 className="card-title ">Governance</h4>
                             <p className="card-text">
