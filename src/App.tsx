f49bd8a181b60bf14b302f652330c51864e27954
Anaiq <>
2023-02-11 20:16:56 -0600

    heroku

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ import { Transaction } from './models/transactionModel';
 import { Stock } from './models/stockModel';
 
 
-// const kBaseUrl = 'https://esg-invested-back.herokuapp.com';
-const kBaseUrl = 'http://localhost:5000';
+const kBaseUrl = 'https://esg-invested-back.herokuapp.com';
+// const kBaseUrl = 'http://localhost:5000';
 
   // convert from API functions goes here:
 const convertExchangeFromApi = (apiExchangeStock:ExchangeStockApi) => {
