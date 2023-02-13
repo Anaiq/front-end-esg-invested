271886aeae62ab31c7bfcd3f4db1bbf126067617
Anaiq <>
2023-02-13 11:04:52 -0600

    added percent component for goal planning page

---
+++ src/components/Percent.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface IPercentProps {
+}
+
+const Percent
+: React.FunctionComponent<IPercentProps> = (props) => {
+    return (
+        <div className='container'>
+            <h4>Max 100%</h4>
+        </div>
+    );
+};
+
+export default Percent
+;
